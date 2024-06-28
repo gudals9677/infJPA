92f62ec4f143c6b20ed44df3582f1834480fc6de
gudals <gudals9677@gmail.com>
2024-06-28 16:37:28 +0900

    JPA Entity

---
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,34 @@
+package hellojpa;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        //트랜잭션 처리 해줘야 오류가 안뜸
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member findMember = em.find(Member.class, 1L);
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
+                    .getResultList();
+
+            tx.commit();
+        } catch(Exception e){
+            tx.rollback();
+        } finally {
+            //code
+            em.close();
+        }
+        emf.close();
+    }
+}
