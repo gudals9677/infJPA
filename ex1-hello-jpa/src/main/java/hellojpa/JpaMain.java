2fe33773f5b391f639d14e80538220e0bdb72ffe
gudals9677 <gudals9677@gmail.com>
2024-07-09 22:16:08 +0900

    값 타입

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 public class JpaMain {
 
@@ -18,12 +19,26 @@ public class JpaMain {
 
         try {
 
-            Address address = new Address("city", "street", 10000);
-
             Member member = new Member();
             member.setUsername("member1");
-            member.setHomeAddress(address);
+            member.setHomeAddress(new Address("homeCity", "street", 10000));
+
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("족발");
+            member.getFavoriteFoods().add("피자");
+
             em.persist(member);
+
+            em.flush();
+            em.clear();
+            System.out.println("====================================");
+            Member findMember = em.find(Member.class, member.getId());
+
+            Address a = findMember.getHomeAddress();
+            findMember.setHomeAddress(new Address("newCity", "street",20000));
+            
+            findMember.getFavoriteFoods().remove("족발");
+            findMember.getFavoriteFoods().add("짜장면");
 
             tx.commit();
         } catch(Exception e){
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -3,9 +3,13 @@ package hellojpa;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
-public class Member extends BaseEntity{
+public class Member {
 
     @Id @GeneratedValue
     @Column(name = "MEMBER_ID")
@@ -15,10 +19,19 @@ public class Member extends BaseEntity{
     private String username;
 
     @Embedded
-    private Period workPeriod;
-
-    @Embedded
     private Address homeAddress;
+
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
+
+//    @ElementCollection
+//    @CollectionTable(name = "ADDRESS", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+//    private List<Address> addressHistory = new ArrayList<>();
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "MEMBER_ID")
+    private List<AddressEntity> address = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -36,19 +49,27 @@ public class Member extends BaseEntity{
         this.username = username;
     }
 
-    public Period getWorkPeriod() {
-        return workPeriod;
-    }
-
-    public void setWorkPeriod(Period workPeriod) {
-        this.workPeriod = workPeriod;
-    }
-
     public Address getHomeAddress() {
         return homeAddress;
     }
 
     public void setHomeAddress(Address homeAddress) {
         this.homeAddress = homeAddress;
+    }
+
+    public Set<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public void setFavoriteFoods(Set<String> favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
+    }
+
+    public List<AddressEntity> getAddress() {
+        return address;
+    }
+
+    public void setAddress(List<AddressEntity> address) {
+        this.address = address;
     }
 }
